eb2a39e0d2e4daa3a98e18a7059b97f2dfbbdbe7
Gladtbam <49943258+Gladtbam@users.noreply.github.com>
2021-11-19 23:35:32 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,17 +16,17 @@
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 #应用过滤插件
-git clone https://github.com/destan19/OpenAppFilter package/OpenAppFilter
+#git clone https://github.com/destan19/OpenAppFilter package/OpenAppFilter
 #KoolProxyR去广告插件
-git clone https://github.com/jefferymvp/luci-app-koolproxyR package/luci-app-koolproxyR
+#git clone https://github.com/jefferymvp/luci-app-koolproxyR package/luci-app-koolproxyR
 #京东签到插件及其node-request依赖
-git clone https://github.com/jerrykuku/node-request  package/node-request 
-git clone https://github.com/jerrykuku/luci-app-jd-dailybonus package/luci-app-jd-dailybonus
+#git clone https://github.com/jerrykuku/node-request  package/node-request 
+#git clone https://github.com/jerrykuku/luci-app-jd-dailybonus package/luci-app-jd-dailybonus
 #clash插件
-git clone https://github.com/frainzy1477/luci-app-clash.git package/luci-app-clash
+#git clone https://github.com/frainzy1477/luci-app-clash.git package/luci-app-clash
 #mentohust
-git clone https://github.com/KyleRicardo/MentoHUST-OpenWrt-ipk.git package/mentohust
-git clone https://github.com/BoringCat/luci-app-mentohust.git package/luci-app-mentohust
+#git clone https://github.com/KyleRicardo/MentoHUST-OpenWrt-ipk.git package/mentohust
+#git clone https://github.com/BoringCat/luci-app-mentohust.git package/luci-app-mentohust
 #git clone https://github.com/fw876/helloworld package/helloworld
 #git clone https://github.com/Lienol/openwrt-package package/lienol
 #git clone https://github.com/kenzok8/openwrt-packages package/kenzo
